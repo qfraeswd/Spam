10d4a8fc550d34624d44f8179b5c753c621df5b7
qfraeswd <47043267+qfraeswd@users.noreply.github.com>
2019-04-10 13:35:47 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,24 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Mehdi");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("565498507377770510")
+setInterval(function() {
+channel.send("SpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpamSpam");
+}, 30)
+})
+
+client.on('message', message => {
+    if(message.content === '-كريدت'){
+        message.channel.send('#daily')
+    }
+});
+client.on('message', message => {
+    if(message.content === '-رصيد'){
+        message.channel.send('#credits')
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
